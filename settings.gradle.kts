1cfb1ca72c504c5026365df3745a4a52a11de984
Fawzy <mohamed.fawzysobhy1@vodafone.com>
2024-03-08 20:10:50 +0200

    feat: init project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WeatherApp"
+include(":app")
+include(":core")
+include(":features")
+include(":data")
+include(":features:currentweather")
+include(":features:forecasting")
+include(":features:searching")
